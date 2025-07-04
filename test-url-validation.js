a91d313cd686f5bd8090350553b0df570256615b
Muhammad Asim <MuhammadAsim2222@yahoo.com>
2025-07-04 13:03:03 +0500

    updates

--- test-url-validation.js
+++
@@ -1,7 +0,0 @@
-const validUrl = require('valid-url');
-
-console.log('Testing URL validation:');
-console.log('https://freeCodeCamp.org:', validUrl.isUri('https://freeCodeCamp.org'));
-console.log('https://freecodecamp.org:', validUrl.isUri('https://freecodecamp.org'));
-console.log('http://www.example.com:', validUrl.isUri('http://www.example.com'));
-console.log('invalid-url:', validUrl.isUri('invalid-url')); 
